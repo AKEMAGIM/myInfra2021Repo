7e7ce92ea387dd59524da8be37689d9293ac1dd4
johndevops <144555846+AKEMAGIM@users.noreply.github.com>
2023-11-03 16:18:54 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-s3-bucket-for-tf-state"
+    bucket = "myawsbucketakem"
     key = "main"
     region = "us-east-2"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    dynamodb_table = "Mydynamodbakem"
   }
 }
